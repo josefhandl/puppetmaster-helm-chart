41b4d951d1d682f52afb8aa1c5b67a17984d8390
Josef Handl <handljosef@gmail.com>
2025-01-30 09:56:51 +0000

    solve problem with dns resolving

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --user 999:999 --cap-drop ALL --network=host -h "playground.internal" --rm -it -p 8140:8140 --name=ps ps /bin/bash
+docker run --user 999:999 --cap-drop ALL -h "playground.internal" --rm -it -p 8140:8140 --name=ps ps /bin/bash
+
