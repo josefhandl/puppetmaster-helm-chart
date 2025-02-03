29de5dc26ac15ed0f326f01fbdb60af01e243b60
Josef Handl <handljosef@gmail.com>
2025-02-03 16:16:53 +0000

    fix puppet server start script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,16 +10,14 @@ mkdir -p "${PUPPET_PATH}/puppetserver/ca/signed"
 # Substiture vars (set hostname) in puppet.conf file
 envsubst < "${PUPPET_PATH}/puppet/puppet.conf" | sponge "${PUPPET_PATH}/puppet/puppet.conf"
 
-if [ -z "$(ls -A /opt/puppet-cert/ca/)" ]; then
+if [ -n "$(ls /opt/puppet-cert/ca/)" ]; then
     cp /opt/puppet-cert/ca/*     "${PUPPET_PATH}/puppetserver/ca/"
     cp /opt/puppet-cert/signed/* "${PUPPET_PATH}/puppetserver/ca/signed"
+    echo "Puppet certs was loaded from Kubernetes Secrets"
 else
     puppetserver ca setup --config "$PUPPET_CONFIG"
 fi
 
-sleep infinity
-
 /opt/puppetlabs/bin/puppetserver start
 
 tail -f /var/log/puppetlabs/puppetserver/puppetserver.log
-
