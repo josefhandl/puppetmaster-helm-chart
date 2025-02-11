1a74b47c361fe790eaa90b5b0545f848a024a126
Josef Handl <handljosef@gmail.com>
2025-02-11 16:33:47 +0000

    add security context and set uid for puppetdb

--- dockerfiles/db/docker-entrypoint.sh
+++ dockerfiles/db/docker-entrypoint.sh
@@ -21,12 +21,12 @@ if [ -n "$(ls /opt/puppet-cert/private/)" ]; then
     cp /opt/puppet-cert/private/* "${PUPPET_PATH}/puppet/ssl/private_keys/"
 
     # Bootstrap certificates
-    puppet ssl bootstrap
+    puppet ssl bootstrap --config "$PUPPET_CONFIG"
 
     echo "Puppet certs was loaded from Kubernetes Secrets"
 else
     # Bootstrap new private key and request the Puppet Server's CA for signature and wait
-    puppet ssl bootstrap
+    puppet ssl bootstrap --config "$PUPPET_CONFIG"
 
     # When signed, save the private key to the Kubernetes secret
     #puppet-cert-saver db-private
