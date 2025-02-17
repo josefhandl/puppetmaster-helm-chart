99c25129387aa751c1a44d0e3d1357b23ea6723e
Josef Handl <handljosef@gmail.com>
2025-02-17 16:25:27 +0100

    add traps for both Server and DB to deliver terminate signal to the puppet app

--- dockerfiles/server/docker-entrypoint.sh
+++ dockerfiles/server/docker-entrypoint.sh
@@ -21,4 +21,22 @@ fi
 
 puppetserver start --config "$PUPPET_CONFIG"
 
-tail -f /var/log/puppetlabs/puppetserver/puppetserver.log
+tail -f /var/log/puppetlabs/puppetserver/puppetserver.log & pid_tail=$!
+
+# Register trap and wait for signal
+pid_java=""
+while [ -z "$pid_java" ]; do
+    sleep 1
+    pid_java=$(pgrep "java")
+done
+
+_trap () {
+    kill "$pid_java"
+    while ps "$pid_java" > /dev/null ; do
+        sleep 1
+    done
+    kill "$pid_tail"
+}
+
+trap "_trap" SIGINT SIGTERM
+wait "$pid_tail"
