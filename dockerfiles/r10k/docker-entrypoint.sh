ff8cbc01b549975db9c26b562ae984e2cac58063
Josef Handl <handljosef@gmail.com>
2025-03-10 13:39:13 +0100

    add ssh key for r10k container in PuppetServer

--- dockerfiles/r10k/docker-entrypoint.sh
+++ dockerfiles/r10k/docker-entrypoint.sh
@@ -10,6 +10,7 @@ envsubst < "${PUPPET_PATH}/puppet/r10k.yaml" | sponge "${PUPPET_PATH}/puppet/r10
 trap exit SIGINT SIGTERM
 
 while true; do
+    echo "Pulling repository"
     /usr/local/bin/r10k deploy environment --config /etc/puppetlabs/puppet/r10k.yaml
     sleep 900
 done
